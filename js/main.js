a4c86b3e732c249a48766df0449dd0d227a7e765
rbasqual <radwan.basqual@cegedim-activ.com>
2019-06-13 15:31:05 +0100

    'Choose your city' option added

--- js/main.js
+++ js/main.js
@@ -1,101 +1,29 @@
-var isha = localStorage.getItem('Isha');
-var maghreb = localStorage.getItem('Maghreb');	
-var asr = localStorage.getItem('Asr');
-var dohr = localStorage.getItem('Dohr');
-var sobh = localStorage.getItem('Sobh');
-
-let minHour = parseInt(moment(sobh, ['h:m a', 'H:m']).format('HH')) - 1 ;
-let maxHour = parseInt(moment(isha, ['h:m a', 'H:m']).format('HH')) + 2 ;
-let nbMinutesToDisplay = 60*(maxHour - minHour);
-
 function init() {
-	showBar();
-	//window.setInterval(showBar, 30000);
-}
 
-/**
- * Calculer le nombre de minutes entre l'heure de la prière [prayerTime] et minHour du matin
- * @param prayerTime
- * @returns {number}
- */
-function getMinuteByPrayerTime(prayerTime){
-	if( (prayerTime) && (prayerTime != null) ){
-		var minPrayerTime = parseInt(prayerTime.split(":")[0])*60+parseInt(prayerTime.split(":")[1]);
-		minPrayerTime = minPrayerTime - 60 * minHour; // 60 * 3
-		return minPrayerTime;
-	}
-}
-
-/**
- * Calculer le pourcentage des minutes par rapport à 9 (21:00 - 06:00 = 15h => 15 * 60min = 900min => 900 / 100 = 9)
- * @param minPrayerTime
- * @returns {string}
- */
-function getPercentByMinPrayerTime(minPrayerTime){
-	var percentPrayerTime = 100 * minPrayerTime / nbMinutesToDisplay;
-	percentPrayerTime = percentPrayerTime.toFixed(2);
-	return percentPrayerTime;
-}
-
-/**
- * Afficher le timeline
- */
-function showBar(){
-	
-	let cityId = localStorage.getItem('cityId');
-	fetch('https://maroc-salat.herokuapp.com/city/'+cityId+'?lang=fr')
-	.then(response => response.json())
-	.then(data => {
-		let cityName = data.name;
-		$("p").text(cityName+', le '+ moment().format('DD/MM/YYYY'))
-		//"Copyright 2018, Rabat-Morocco_v0.1"
+	var link = document.getElementById('infos');
+	link.addEventListener('dblclick', function() {
+		$("#divParam").removeClass("hideParam");
+		$("#divParam").addClass("showParam");
+		$("#divParam").css("top", "-10px");
 	});
 
-	var minSobh = getMinuteByPrayerTime(sobh);
-	var percentSobh = getPercentByMinPrayerTime(minSobh);
-	$("#salat0").width(percentSobh + "%");
-	
-	var minDohr = getMinuteByPrayerTime(dohr);
-	var percentDohr = getPercentByMinPrayerTime(minDohr-minSobh);
-	$("#salat1").width(percentDohr + "%");
-	
-	var minAsr = getMinuteByPrayerTime(asr);
-	var percentAsr = getPercentByMinPrayerTime(minAsr-minDohr);
-	$("#salat2").width(percentAsr + "%");
-	
-	var minMaghreb = getMinuteByPrayerTime(maghreb);
-	var percentMaghreb = getPercentByMinPrayerTime(minMaghreb-minAsr);
-	$("#salat3").width(percentMaghreb + "%");
-	
-	var minIsha = getMinuteByPrayerTime(isha);
-	var percentIsha = getPercentByMinPrayerTime(minIsha-minMaghreb);
-	$("#salat4").width(percentIsha + "%");
-	
-	var total = parseFloat(percentSobh) + parseFloat(percentDohr) + parseFloat(percentAsr) + parseFloat(percentMaghreb) + parseFloat(percentIsha);
-	var toFullBar = 100 - total;
-	$("#salat5").width(toFullBar.toFixed(2) + "%");	
-	
-	let currentHour = moment().format('HH:mm');
-	var minNow = getMinuteByPrayerTime(currentHour);
-	var hourBarWidth = parseFloat(window.innerWidth * getPercentByMinPrayerTime(minNow) / 100).toFixed(2); 
-	$("#hourBar").width(hourBarWidth+'px');
-	
-	document.getElementById("currentHour").textContent = currentHour;	
-	document.getElementById("salatTime1").textContent = sobh;
-	document.getElementById("salatTime2").textContent = dohr;
-	document.getElementById("salatTime3").textContent = asr;
-	document.getElementById("salatTime4").textContent = maghreb;
-	document.getElementById("salatTime5").textContent = isha;
+	var closeBtn = document.getElementById('closeBtn');
+	closeBtn.addEventListener('click', function() {
+		$("#divParam").removeClass("showParam");
+		$("#divParam").addClass("hideParam");
+		$("#divParam").css("top", "-90px");
+	});
 
-	percentSobh = (parseFloat(percentSobh) + 2.16).toFixed(2);
-	$("#divSalatTime1").width(percentSobh + "%");				
-	$("#divSalatTime2").width((parseFloat(percentDohr) - 0.4).toFixed(2) + "%");
-	$("#divSalatTime3").width((parseFloat(percentAsr) - 0.4).toFixed(2) + "%");
-	$("#divSalatTime4").width((parseFloat(percentMaghreb) - 0.4).toFixed(2) + "%");
-	$("#divSalatTime5").width((parseFloat(percentIsha) - 0.4).toFixed(2) + "%");
+	var select = document.getElementById('choosedCity');
+	select.addEventListener('change', function() {
+		localStorage.setItem('cityId', select.value);
+		localStorage.removeItem('dayNotif');
+		showNotify();
+	});
+	$("#choosedCity").val(localStorage.getItem('cityId'));
+
+	showBars();
 
 }
 
 document.addEventListener('DOMContentLoaded', init);
-
-
